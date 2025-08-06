8cded9beb54a5631ec6313f8200609fcb14a6a27
ManishGupta1456 <guptamanish1456@gmail.com>
2025-08-06 15:58:42 +0630

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,3 @@
-const supabase = supabase.createClient('https://fvfhptpkyntqijtpgdse.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ2ZmhwdHBreW50cWlqdHBnZHNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEzNDk4OTAsImV4cCI6MjA2NjkyNTg5MH0.HFVj4oZ_acMOmLiL7ix-G8DahuWzmL9mqDkuvjr8HRA');
 
 async function login() {
   const email = document.getElementById('email').value;
@@ -31,3 +30,4 @@ async function fetchBooking() {
   document.getElementById('guest-state').value = data.state;
   document.getElementById('amount').value = data.total_amount;
 }
+
